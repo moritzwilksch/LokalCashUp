7ad4c679a4d4d919b550dd296b302dbdba5b4a85
Moritz Wilksch <moritzwilksch@gmail.com>
2020-05-03 14:19:41 +0200

    added selectbox

--- app.py
+++ app.py
@@ -1,14 +1,25 @@
 from flask import Flask
 from flask import render_template
 
+import datetime
+
 app = Flask(__name__)
+
+MALIST = ['Bitte wählen...', 'Sandrina', 'Eric', 'Nochwer']
+TODAY = ".".join([str(getattr(datetime.date.today(), att)) for att in ['day', 'month', 'year']])
+DEFAULT_FIELDS = {
+    'gt100In': '42',
+    'gt100Out': '42,00 €'
+}
 
 
 @app.route('/')
 def hello_world():
-    return render_template('input_form.html')
+    return render_template('input_form.html',
+                           ma_list=MALIST,
+                           today=TODAY,
+                           fields=DEFAULT_FIELDS)
 
 
 if __name__ == '__main__':
     app.run()
-    print('Hello world')
