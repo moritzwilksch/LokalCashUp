bb00198f40263894d1362c076f73a4eeeeb309b8
Moritz Wilksch <moritzwilksch@gmail.com>
2026-02-21 11:46:39 +0100

    polish

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import uvicorn
 
 from app.web import app
 
+APP_PORT = 1605
+
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=1605)
+    uvicorn.run(app, host="0.0.0.0", port=APP_PORT)
